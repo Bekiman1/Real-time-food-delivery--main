212c2b032fb5da57ff4fa109ed2fca0e9dfc7fdd
Bekiman1 <bereketsamuel59@gmail.com>
2025-08-29 21:21:43 +0300

    3

--- src/api/customer/restaurantApi.js
+++ src/api/customer/restaurantApi.js
@@ -1,9 +1,7 @@
 // src/api/restaurantApi.js
 export const fetchNearbyRestaurants = async (lat, lng) => {
   const url =
-    lat && lng
-      ? `https://real-time-food-delivery.onrender.com/api/delivery/restaurants?lat=${lat}&lng=${lng}`
-      : "https://real-time-food-delivery.onrender.com/api/delivery/restaurants";
+    "https://real-time-food-delivery.onrender.com/api/delivery/restaurants";
 
   const response = await fetch(url, {
     method: "GET",
